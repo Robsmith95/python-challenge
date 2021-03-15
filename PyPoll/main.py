fa6efb79e17d696c182f791d1c73347a84760cc7
Robert <robertsmith1995@gmail.com>
2021-03-15 07:41:53 -0700

    Updated CSV Files

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,15 +1,16 @@
-
 import os
 import csv
 
 voting_total = 0
 candidates = []
 candidates_votes = []
+
 #import csv
 csvpath = os.path.join('PyPoll', 'Resources', 'election_data.csv')
 with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     next(csvreader,None) 
+
 #loop through csv
     for row in csvreader:
         voting_total += 1
@@ -23,6 +24,7 @@ with open(csvpath, newline='') as csvfile:
             except:
                 candidates.append(row[2])
                 candidates_votes.append(1)
+
 #display voting results
 results = []
 results.append("Election Results\n-------------------------")
@@ -38,7 +40,8 @@ for i in range(len(candidates)):
     results.append(f"{candidates[i]}: {round(percentage_of_votes,3)} % ({candidates_votes[i]})")
 
 results.append(f"-------------------------\nWinner: {candidates_winner}\n-------------------------")
-#create text file to show results
+
+#create text file to show PyPoll results
 filename = 'PyPollResults.txt'
 with open(filename, 'w') as file:
     for result in results:
